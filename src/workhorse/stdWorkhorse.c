6d5cee9f1c979271ed8df62c96e38f1e3f18a414
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-05-11 21:07:04 +0300

    Fix build issue
    Issue caused by inlining of memset/memcpy or not at all. Causing unknown symbol/multiple definition errors

---
+++ src/workhorse/stdWorkhorse.c
@@ -0,0 +1,22 @@
+#include <stdWorkhorse.h>
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    const char *_src = (char *)src;
+    char *_dest = (char *)dest;
+
+    for (uint64_t i = 0; i < n; i++)
+        _dest[i] = _src[i]; 
+
+    return dest;
+}
+
+void *memset(void *str, int c, size_t n)
+{
+    char *_str = (char *)str;
+
+    for (uint64_t i = 0; i < n; i++)
+        _str[i] = c;
+
+    return str;
+}
