691e6cfce1ed6949c651ec51bd5a32e0ea5b6f8a
humzak711 <humzak711@gmail.com>
2026-06-09 21:39:32 +0100

    stylistic violation

--- arch/ia32e/src/ia32eEmulator.c
+++ arch/ia32e/src/ia32eEmulator.c
@@ -1963,7 +1963,7 @@ ia32eEmulatorFn_t ia32eEmulatorDispatchTable[] = {
     [IA32E_VTX_EXIT_REASON_ENCLS] = ia32eEmulatorUd,
 };
 
-/* hypervisor helpers */
+/* Hypervisor helpers */
 
 static
 void ia32eEmulatorVmcsReset(void)
@@ -2275,7 +2275,7 @@ void ia32eEmulatorRegsReset(ia32eVmexitRegs_t *regs)
     memset(cpu->vtx.areas.vmexitStoreVmentryLoadArea, 0, sizeof(cpu->vtx.areas.vmexitStoreVmentryLoadArea));
 }
 
-/* hypervisor */
+/* Hypervisor */
 
 static 
 bool ia32eEmulatorEventManager(ia32eVmexitRegs_t *regs)
